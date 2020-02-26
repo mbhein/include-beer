2ec87b7dc270c5153895ea983ded9413c4ffa91a
mbhein <matthew.hein@gmail.com>
2020-02-26 17:42:22 -0600

    fix: update simpletest to use DHT11

--- dht_simpletest.py
+++ dht_simpletest.py
@@ -3,7 +3,7 @@ import board
 import adafruit_dht
  
 # Initial the dht device, with data pin connected to:
-dhtDevice = adafruit_dht.DHT22(board.D26)
+dhtDevice = adafruit_dht.DHT11(board.D26)
  
 while True:
     try:

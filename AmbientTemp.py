2eca7548f7a4ce5fc59d78c002f30e443a876dc4
mbhein <matthew.hein@gmail.com>
2020-03-22 17:02:38 -0500

    feat: refactor ambient to use python lib

--- AmbientTemp.py
+++ AmbientTemp.py
@@ -7,21 +7,21 @@
  Requires DHT11 sensor for RPi
 
 """
+import board
+import adafruit_dht
 
-import Adafruit_DHT
-
-
-def readAmbient(pin):
-    #Read Ambient Temp/Humidity from DHT11
-    humidity, temp = Adafruit_DHT.read_retry(11, pin)
-    temp = temp * 9/5.0 + 32
-    return temp, humidity
+def readAmbient(data_pin_connection):
+    dht_device = adafruit_dht.DHT11(data_pin_connection)
+    temperature_c = dht_device.temperature
+    temperature_f = temperature_c * (9 / 5) + 32
+    humidity = dht_device.humidity
+    return temperature_f, humidity
 
 
 
 def main():
-    defaultPin = 25
-    ambientTemp, ambientHumidity = readAmbient(defaultPin)
+    data_pin_connection = board.D26
+    ambientTemp, ambientHumidity = readAmbient(data_pin_connection)
     print('Ambient Temperature: ' + str(ambientTemp))
     print('Ambient Humidity: ' + str(ambientHumidity))
 
--- SDK/Adafruit_DHT.py
+++ SDK/Adafruit_DHT.py
@@ -1,28 +1,15 @@
 #!/usr/bin/env python3
 
-""" Used to mimic Adafruit_DHT plugin
+""" Used to mimic adafruit_dht module
 
     Usage:
-        python3 Adafruit_DHT.py
+        python3 adafruit_dht.py
 
-    Adafruit_DHT.read_retry(11, pin) returns 60 and 58
+    
 
 """
 
-def read_retry(x,y):
-    humidity = 58
-    temp = 20
-    return humidity, temp
-
-
-
-def main():
-    x = 11
-    y = 25
-    ambientTemp, ambientHumidity = read_retry(x,y)
-    print('Ambient Temperature: ' + str(ambientTemp))
-    print('Ambient Humidity: ' + str(ambientHumidity))
-
-
-if __name__ == '__main__':
-    main()
+class DHT11(object):
+    def __init__(self, data_pin_connection):
+        self.temperature = 20
+        self.humidity = 58

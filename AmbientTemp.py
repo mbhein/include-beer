03294f9a52a960ea1124231fd4ed0a9252a9df51
mbhein <matthew.hein@gmail.com>
2020-03-22 17:33:19 -0500

    feat: add exception handling

--- AmbientTemp.py
+++ AmbientTemp.py
@@ -12,10 +12,14 @@ import adafruit_dht
 
 def readAmbient(data_pin_connection):
     dht_device = adafruit_dht.DHT11(data_pin_connection)
-    temperature_c = dht_device.temperature
-    temperature_f = temperature_c * (9 / 5) + 32
-    humidity = dht_device.humidity
-    return temperature_f, humidity
+    try:
+        temperature_c = dht_device.temperature
+        temperature_f = temperature_c * (9 / 5) + 32
+        humidity = dht_device.humidity
+        return temperature_f, humidity
+    except RuntimeError as error:
+        # Errors happen fairly often, DHT's are hard to read, just keep going
+        return (error.args[0])
 
 
 

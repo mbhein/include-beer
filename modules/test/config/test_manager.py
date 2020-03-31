abb0d1a1c6213f2f1b09b3e4e8a035a9e7ca7fa6
mbhein <matthew.hein@gmail.com>
2020-03-30 20:09:51 -0500

    feat: intro config module and test framework

---
+++ modules/test/config/test_manager.py
@@ -0,0 +1,15 @@
+import pytest
+import os
+import sys
+import json
+import modules.config.manager as cfg_mgr
+
+
+def test_config_mgr_class():
+    os.environ["INCLUDE_BEER_CONFIG"] = ('%s/test.cfg' % os.path.dirname(__file__))
+    cfg = cfg_mgr.ConfigManager()
+    print('')
+    print(cfg._default_def)
+    print(cfg._option_type_def)
+    print(cfg._base_config_def)
+    print(cfg._config_file_def)

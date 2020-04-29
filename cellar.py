dc12d51102440866ae1f32b2515157ae36b9adcf
mbhein <matthew.hein@gmail.com>
2020-04-28 19:59:29 -0500

    feat: intro log and stat buffer

--- cellar.py
+++ cellar.py
@@ -14,16 +14,24 @@ import modules.config.manager as cfg_mgr
 import modules.config.logging as logger
 
 def main():
+    # init attributes
+    log_buffer = []
+    stat_buffer = []
+    _date = time.strftime("%Y-%m-%d", time.localtime())
+    stat_buffer.append(_date)
+    _time = time.strftime("%H:%M:%S", time.localtime())
+    stat_buffer.append(_time)
 
     # Set config object
     config = cfg_mgr.ConfigManager()
-    date_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+    
 
     # Set logging objects
     log_dir = os.path.expanduser(config.defaults.log_dir)
     log_file = os.path.join(log_dir, 'cellar.log')
     log_this = logger.load_logger('include-beer.cellar', log_file, config.defaults.debug)
     log_this.debug('Opening cellar')
+    log_buffer.append('Opening cellar')
     
     # Load ambient sensor module
     ambient_sensor = config.defaults.ambient_sensor
@@ -37,8 +45,12 @@ def main():
     # Get ambient temperature and humidity
     ambient_temp, ambient_humidity = a_sensor.read(config.defaults.ambient_pin)
     log_this.debug('Ambient Temperature: ' + str(ambient_temp))
+    log_buffer.append('A/T: ' + str(ambient_temp))
     log_this.debug('Ambient Humidity: ' + str(ambient_humidity))
+    log_buffer.append('A/H: ' + str(ambient_humidity))
 
+    # clear log_buffer
+    log_this.info(' | '.join(map(str, log_buffer)))
     
 
 

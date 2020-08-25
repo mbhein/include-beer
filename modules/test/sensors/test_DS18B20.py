97c1b18b8c699168f86215b42a42fae128f5f3f4
mbhein <matthew.hein@gmail.com>
2020-08-25 18:32:53 -0500

    feat: move to kwargs for DS18B20 module

--- modules/test/sensors/test_DS18B20.py
+++ modules/test/sensors/test_DS18B20.py
@@ -4,18 +4,22 @@ import modules.sensors.DS18B20 as sensor
 
 
 def test_found(capsys):
-    probe_id = '28-2018'
-    base_dir = './SDK'
-    data_file = 'w1_slave'
-    temp = sensor.read(probe_id, base_dir, data_file)
+    kwargs = { 'serial_number': '28-2018', 
+        'base_dir': './SDK',
+        'data_file': 'w1_slave',
+        'temperature_scale': 'f'
+        }
+    temp = sensor.read(**kwargs)
     assert type(temp) is float
     print('Sensor Temperature: ' + str(temp))
 
 
 def test_not_found(capsys):
-    probe_id = '28-2020'
-    base_dir = './SDK'
-    data_file = 'w1_slave'
-    temp = sensor.read(probe_id, base_dir, data_file)
+    kwargs = {'serial_number': '28-2020',
+              'base_dir': './SDK',
+              'data_file': 'w1_slave',
+              'temperature_scale': 'f'
+              }
+    temp = sensor.read(**kwargs)
     assert type(temp) is str
     print('Sensor Temperature: ' + str(temp))

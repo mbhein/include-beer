8f4b2537701737783f365d24690fbb0f31dffd68
mbhein <matthew.hein@gmail.com>
2020-08-25 20:10:49 -0500

    feat: handle multiple probes across multiple sessions

--- tests/test_fermentation.py
+++ tests/test_fermentation.py
@@ -4,6 +4,8 @@ import os
 
 def test_ferment(capsys):
     os.environ["INCLUDE_BEER_CONFIG"] = os.path.abspath('SDK/sdk.cfg')
+    os.environ["INCLUDE_BEER_CONFIG_SESSION_FILE"] = os.path.abspath('SDK/sdk_sessions.yml')
+
     print(os.environ["INCLUDE_BEER_CONFIG"])
     fermentation.main()
 
